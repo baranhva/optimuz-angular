043a24e3b2d7211a888e3eb80351fa2d6dc0fb5f
Christiaan Hoijtink <christiaanhoijtink@gmail.com>
2021-03-18 15:35:29 +0100

    Create core module and import it in app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {LoginComponent} from './login/login.component';
 import {SharedModule} from './shared/shared.module';
+import {CoreModule} from './core/core.module';
 
 @NgModule({
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    CoreModule,
     SharedModule
   ],
   declarations: [
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,43 @@
+import {NgModule, Optional, SkipSelf} from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
+import {RouterModule} from '@angular/router';
+import {ApiPrefixInterceptor} from './interceptor/api-prefix.interceptor';
+import {JwtInterceptor} from './interceptor/jwt.interceptor';
+import {TokenInterceptor} from './interceptor/token.interceptor';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    HttpClientModule,
+    RouterModule
+  ],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ApiPrefixInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
+      multi: true
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true
+    }
+  ]
+})
+export class CoreModule {
+
+  constructor(@Optional() @SkipSelf() parentModule: CoreModule) {
+    // Import guard
+    if (parentModule) {
+      throw new Error(`${parentModule} has already been loaded. Import Core module in the AppModule only.`);
+    }
+  }
+}
