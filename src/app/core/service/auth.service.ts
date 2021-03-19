7c4edf214297240de2d6d137491577fd5239f2c5
Christiaan Hoijtink <christiaanhoijtink@gmail.com>
2021-03-19 13:42:47 +0100

    Store the payload of the access token in the auth service. Create guard for blocking access to routes which are not the specified user type

--- src/app/core/service/auth.service.ts
+++ src/app/core/service/auth.service.ts
@@ -1,7 +1,23 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import {catchError, mapTo, tap} from 'rxjs/operators';
+import {catchError, map, mapTo, tap} from 'rxjs/operators';
 import {Observable, of} from 'rxjs';
+import {JwtHelperService} from '@auth0/angular-jwt';
+import {CanLoad, Route, Router} from '@angular/router';
+
+const jwtHelper = new JwtHelperService();
+
+export type UserType = "ADMIN" | "CARETAKER" | "PATIENT";
+
+export const AdminType: UserType = "ADMIN";
+export const CaretakerType: UserType = "CARETAKER";
+export const PatientType: UserType = "PATIENT";
+
+interface TokenPayload {
+  id?: number;
+  email?: string;
+  type?: UserType;
+}
 
 export interface Tokens {
   accessToken: string;
@@ -11,12 +27,14 @@ export interface Tokens {
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
+export class AuthService implements CanLoad {
+
+  private token: TokenPayload;
 
   private readonly ACCESS_TOKEN_STORAGE_KEY: string = 'ACCESS_TOKEN';
   private readonly REFRESH_TOKEN_STORAGE_KEY: string = 'REFRESH_TOKEN';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private router: Router) { }
 
   getAccessToken(): string {
     return localStorage.getItem(this.ACCESS_TOKEN_STORAGE_KEY);
@@ -29,8 +47,14 @@ export class AuthService {
   login(email: string, password: string): Observable<boolean> {
     return this.http.post<Tokens>(`/auth/login`, {email, password})
       .pipe(
-        tap(this.storeTokens),
-        mapTo(true),
+        map((tokens: Tokens) => {
+          this.storeTokens(tokens);
+          this.decodeToken(tokens);
+          return true;
+        }),
+        // tap(this.storeTokens),
+        // tap(this.decodeToken),
+        // mapTo(true),
         catchError(error => {
           alert(error.error);
           return of(false);
@@ -44,6 +68,10 @@ export class AuthService {
     }).pipe(tap(this.storeTokens));
   }
 
+  private decodeToken(tokens: Tokens) {
+    this.token = jwtHelper.decodeToken(tokens.accessToken);
+  }
+
   private storeTokens(tokens: Tokens) {
     localStorage.setItem(this.ACCESS_TOKEN_STORAGE_KEY, tokens.accessToken);
     localStorage.setItem(this.REFRESH_TOKEN_STORAGE_KEY, tokens.refreshToken);
@@ -52,5 +80,36 @@ export class AuthService {
   private removeTokens() {
     localStorage.removeItem(this.ACCESS_TOKEN_STORAGE_KEY);
     localStorage.removeItem(this.REFRESH_TOKEN_STORAGE_KEY);
+  }
+
+  isAdmin(): boolean {
+    return this.isUserType(AdminType);
+  }
+
+  isCaretaker(): boolean {
+    return this.isUserType(CaretakerType);
+  }
+
+  private isUserType(type: UserType): boolean {
+    console.log(`this.token: ${this.token}`)
+    console.log(`this.token: ${JSON.stringify(this.token, null, 4)}`)
+    return this.token?.type === type;
+  }
+
+  isLoggedIn(): boolean {
+    return !jwtHelper.isTokenExpired(this.getAccessToken());
+  }
+
+  canLoad(route: Route): boolean {
+    if (!!route.data['type'] && this.isUserType(route.data['type'])) {
+      return true;
+    } else {
+      if (!this.isLoggedIn()) {
+        this.router.navigate(['']);
+        return true;
+      } else {
+        return false;
+      }
+    }
   }
 }
