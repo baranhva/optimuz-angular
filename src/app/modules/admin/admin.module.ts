26c4a0168fa612bd3928655689ac4c867dfa6430
Christiaan Hoijtink <christiaanhoijtink@gmail.com>
2021-03-19 12:59:22 +0100

    Create dashboard route and add admin routes to admin module

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -1,12 +1,18 @@
 import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
+
+import {AdminRoutes} from './admin.routes';
+
 import {SharedModule} from '../../shared/shared.module';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import {DashboardComponent} from './pages/dashboard/dashboard.component';
 
 
 @NgModule({
   imports: [
+    RouterModule.forChild(AdminRoutes),
     SharedModule
   ],
   declarations: [DashboardComponent]
 })
-export class AdminModule { }
+export class AdminModule {
+}
