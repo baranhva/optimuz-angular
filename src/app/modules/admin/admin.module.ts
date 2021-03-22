157e9a751cdab601fe3384f2d96e725f8b5ba5ef
Christiaan Hoijtink <christiaanhoijtink@gmail.com>
2021-03-22 18:16:38 +0100

    Use layout as top component in admin. Create admin sidebar items and provide this in the module

--- src/app/modules/admin/admin.module.ts
+++ src/app/modules/admin/admin.module.ts
@@ -5,6 +5,8 @@ import {AdminRoutes} from './admin.routes';
 
 import {SharedModule} from '../../shared/shared.module';
 import {DashboardComponent} from './pages/dashboard/dashboard.component';
+import {LAYOUT_SIDEBAR_ITEMS} from '../../shared/components/layout/layout.tokens';
+import {AdminSidebarItems} from './admin.sidebar-items';
 
 
 @NgModule({
@@ -12,7 +14,10 @@ import {DashboardComponent} from './pages/dashboard/dashboard.component';
     RouterModule.forChild(AdminRoutes),
     SharedModule
   ],
-  declarations: [DashboardComponent]
+  declarations: [DashboardComponent],
+  providers: [
+    {provide: LAYOUT_SIDEBAR_ITEMS, useValue: AdminSidebarItems}
+  ]
 })
 export class AdminModule {
 }
--- src/app/modules/admin/admin.routes.ts
+++ src/app/modules/admin/admin.routes.ts
@@ -1,9 +1,17 @@
 import {Routes} from '@angular/router';
+import {LayoutComponent} from '../../shared/components/layout/layout.component';
 import {DashboardComponent} from './pages/dashboard/dashboard.component';
 
 export const AdminRoutes: Routes = [
   {
     path: '',
-    component: DashboardComponent
+    component: LayoutComponent,
+    children: [
+      {path: '', pathMatch: 'full', redirectTo: 'dashboard'},
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ]
   }
 ];
