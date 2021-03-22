1fb438e2e2d5d54cc5cfe1a6d1fdbb23621ee882
Christiaan Hoijtink <christiaanhoijtink@gmail.com>
2021-03-22 19:15:49 +0100

    Add dashboard as route value and make the screen full height for layout

--- src/app/modules/admin/admin.sidebar-items.ts
+++ src/app/modules/admin/admin.sidebar-items.ts
@@ -1,7 +1,7 @@
 import {SidebarItem} from '../../shared/components/layout/sidebar-item';
 
 export const AdminSidebarItems: SidebarItem[] = [
-  {title: `Dashboard`, route: '', icon: 'dashboard'},
+  {title: `Dashboard`, route: 'dashboard', icon: 'dashboard'},
   {title: `Users`, route: 'users', icon: 'supervised_user_circle'},
   {title: `Settings`, route: 'settings', icon: 'manage_accounts'}
 ];
