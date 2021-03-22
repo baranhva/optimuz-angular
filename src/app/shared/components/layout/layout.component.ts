957c8059414e6f63905fd8bfe425d45a3d6f24a3
Christiaan Hoijtink <christiaanhoijtink@gmail.com>
2021-03-22 23:19:18 +0100

    Add logout button to nav bar in layout that calls the new logout function

--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -1,6 +1,7 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {SidebarItem} from './sidebar-item';
 import {LAYOUT_SIDEBAR_ITEMS} from './layout.tokens';
+import {AuthService} from '../../../core/service/auth.service';
 
 @Component({
   selector: 'opt-layout',
@@ -9,7 +10,7 @@ import {LAYOUT_SIDEBAR_ITEMS} from './layout.tokens';
 })
 export class LayoutComponent implements OnInit {
 
-  constructor(@Inject(LAYOUT_SIDEBAR_ITEMS) public sidebarItems: SidebarItem[] = []) {
+  constructor(@Inject(LAYOUT_SIDEBAR_ITEMS) public sidebarItems: SidebarItem[] = [], public authService: AuthService) {
   }
 
   ngOnInit(): void {
