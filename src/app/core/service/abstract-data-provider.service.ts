dd695683c647a22235ae71db3568b9e87295a94d
Christiaan Hoijtink <christiaanhoijtink@gmail.com>
2021-03-24 14:13:58 +0100

    Create mechanism for loading data once

---
+++ src/app/core/service/abstract-data-provider.service.ts
@@ -0,0 +1,88 @@
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, CanActivate, CanActivateChild, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {Observable, ReplaySubject} from 'rxjs';
+
+@Injectable()
+export abstract class AbstractDataProviderService implements CanActivate, CanActivateChild {
+
+  private isProviding: boolean = false;
+  private isInit: boolean = false;
+
+  protected initialized: ReplaySubject<any> = new ReplaySubject<any>(1);
+
+  public initialisation(): Observable<any> {
+    return this.initialized.asObservable();
+  }
+
+  abstract init(): void;
+
+  abstract destroy(): void;
+
+  protected reset() {
+    if (this.initialized) {
+      this.initialized.complete();
+    }
+
+    this.initialized = new ReplaySubject<any>(1);
+  };
+
+  protected startDataProviding(): void {
+    this.isProviding = true;
+  }
+
+  public isDataInit(): boolean {
+    return this.isInit;
+  }
+
+  public isProvidingData(): boolean {
+    return this.isProviding;
+  }
+
+  public hasNotStartedProvidingData(): boolean {
+    return !this.isProvidingData() && !this.isDataInit();
+  }
+
+  canActivate(route?: ActivatedRouteSnapshot, state?: RouterStateSnapshot): Observable<boolean> {
+    return new Observable(observer => {
+      if (!this.isDataInit()) {
+        if (this.hasNotStartedProvidingData()) {
+          this.startDataProviding();
+          this.init();
+        }
+
+        this.initialisation().subscribe(
+          () => observer.next(true),
+          err => observer.next(false),
+          () => observer.complete()
+        );
+      } else {
+        observer.next(true);
+        observer.complete();
+      }
+    });
+  }
+
+  canActivateChild(childRoute: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    return this.canActivate(childRoute, state);
+  }
+
+  canDeactivate(component: any, currentRoute: ActivatedRouteSnapshot, currentState: RouterStateSnapshot, nextState?: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    if (this.isDataInit()) {
+      this.destroy();
+      this.resetDataIsInit();
+      this.reset();
+    }
+    return true;
+  }
+
+  protected setDataIsInit(): void {
+    this.isInit = true;
+    this.initialized.next(true);
+  }
+
+  protected resetDataIsInit() {
+    this.isInit = false;
+    this.isProviding = false;
+    this.initialized.complete();
+  }
+}
